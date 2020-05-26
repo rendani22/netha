43c60c2c7facf5aabe8caad74377ca831e6abf95
shiluva-fortunate <shiluva1202@gmail.com>
2020-05-26 11:49:38 +0200

    update: Manufacturing page done

--- assets/js/layer.js
+++ assets/js/layer.js
@@ -2,56 +2,54 @@
 
 //--------------------1-----------------------------------------
 
-$('.testh').mouseover(function(){
-    console.log("hi there");
-    $(this).addClass('card-show');
-    console.log(this);
-    $('.layer-content-container1').addClass('card-show');
-})
-
-$('#image-card1').mouseleave(function(){
-    $('.card-layer1').removeClass('card-show');
-    $('.layer-content-container1').removeClass('card-show');
-})
+    $('#image-card1').mouseover(function(){
+        $('.card-layer1').addClass('card-show');
+        $('.layer-content-container1').addClass('card-show');
+    })
+    
+    $('#image-card1').mouseleave(function(){
+        $('.card-layer1').removeClass('card-show');
+        $('.layer-content-container1').removeClass('card-show');
+    })
 
 //---------------------2----------------------------------------
 
-// $('#image-card2').mouseover(function(){
-//     console.log("hi there");
-//     $('.card-layer2').addClass('card-show');
-//     $('.layer-content-container2').addClass('card-show');
-// })
+$('#image-card2').mouseover(function(){
+    console.log("hi there");
+    $('.card-layer2').addClass('card-show');
+    $('.layer-content-container2').addClass('card-show');
+})
 
-// $('#image-card2').mouseleave(function(){
-//     $('.card-layer2').removeClass('card-show');
-//     $('.layer-content-container2').removeClass('card-show');
-// })
+$('#image-card2').mouseleave(function(){
+    $('.card-layer2').removeClass('card-show');
+    $('.layer-content-container2').removeClass('card-show');
+})
 
 //----------------------3---------------------------------------
 
-// $('#image-card3').mouseover(function(){
-//     console.log("hi there");
-//     $('.card-layer3').addClass('card-show');
-//     $('.layer-content-container3').addClass('card-show');
-// })
+$('#image-card3').mouseover(function(){
+    console.log("hi there");
+    $('.card-layer3').addClass('card-show');
+    $('.layer-content-container3').addClass('card-show');
+})
 
-// $('#image-card3').mouseleave(function(){
-//     $('.card-layer3').removeClass('card-show');
-//     $('.layer-content-container3').removeClass('card-show');
-// })
+$('#image-card3').mouseleave(function(){
+    $('.card-layer3').removeClass('card-show');
+    $('.layer-content-container3').removeClass('card-show');
+})
 
 //----------------------4---------------------------------------
 
-// $('#image-card4').mouseover(function(){
-//     console.log("hi there");
-//     $('.card-layer4').addClass('card-show');
-//     $('.layer-content-container4').addClass('card-show');
-// })
+$('#image-card4').mouseover(function(){
+    console.log("hi there");
+    $('.card-layer4').addClass('card-show');
+    $('.layer-content-container4').addClass('card-show');
+})
 
-// $('#image-card4').mouseleave(function(){
-//     $('.card-layer4').removeClass('card-show');
-//     $('.layer-content-container4').removeClass('card-show');
-// })
+$('#image-card4').mouseleave(function(){
+    $('.card-layer4').removeClass('card-show');
+    $('.layer-content-container4').removeClass('card-show');
+})
 
 //----------------------5---------------------------------------
 
@@ -108,12 +106,98 @@ $('#image-card8').mouseleave(function(){
 
 //pop-up
 
-$(document).on('click', '#button8', function(){
+//-------------------hydro cyclones -----------------------------
+
+$(document).on('click', '#button1', function(){
     $('.pop-up-containers').addClass('card-show');
-    $('.pop-up').addClass('card-show');
+    $('#hydro').addClass('card-show');
 })
 
 $('.cancels').on('click', function(){
     $('.pop-up-containers').removeClass('card-show');
-    $('.pop-up').removeClass('card-show');
+    $('#hydro').removeClass('card-show');
+})
+
+//-------------------transfer chutes-----------------------------
+
+$(document).on('click', '#button2', function(){
+    $('.pop-up-containers').addClass('card-show');
+    $('#trans').addClass('card-show');
+})
+
+$('.cancels').on('click', function(){
+    $('.pop-up-containers').removeClass('card-show');
+    $('#trans').removeClass('card-show');
+})
+
+//-------------------inter-stage screens---------------------------
+
+$(document).on('click', '#button3', function(){
+    $('.pop-up-containers').addClass('card-show');
+    $('#inter').addClass('card-show');
+})
+
+$('.cancels').on('click', function(){
+    $('.pop-up-containers').removeClass('card-show');
+    $('#inter').removeClass('card-show');
+})
+
+//---------------------conveyor idlers-----------------------------
+
+$(document).on('click', '#button4', function(){
+    $('.pop-up-containers').addClass('card-show');
+    $('#conveyor').addClass('card-show');
+})
+
+$('.cancels').on('click', function(){
+    $('.pop-up-containers').removeClass('card-show');
+    $('#conveyor').removeClass('card-show');
+})
+
+//-----------------------screen panels-----------------------------
+
+$(document).on('click', '#button5', function(){
+    $('.pop-up-containers').addClass('card-show');
+    $('#screen').addClass('card-show');
+})
+
+$('.cancels').on('click', function(){
+    $('.pop-up-containers').removeClass('card-show');
+    $('#screen').removeClass('card-show');
+})
+
+//-----------------------spray nozzles-----------------------------
+
+$(document).on('click', '#button6', function(){
+    $('.pop-up-containers').addClass('card-show');
+    $('#spray').addClass('card-show');
+})
+
+$('.cancels').on('click', function(){
+    $('.pop-up-containers').removeClass('card-show');
+    $('#spray').removeClass('card-show');
+})
+
+//-----------------------hdpe pipes---------------------------------
+
+$(document).on('click', '#button7', function(){
+    $('.pop-up-containers').addClass('card-show');
+    $('#pipe').addClass('card-show');
+})
+
+$('.cancels').on('click', function(){
+    $('.pop-up-containers').removeClass('card-show');
+    $('#pipe').removeClass('card-show');
+})
+
+//-----------------------poly-u liners---------------------------------
+
+$(document).on('click', '#button8', function(){
+    $('.pop-up-containers').addClass('card-show');
+    $('#poly').addClass('card-show');
+})
+
+$('.cancels').on('click', function(){
+    $('.pop-up-containers').removeClass('card-show');
+    $('#poly').removeClass('card-show');
 })
